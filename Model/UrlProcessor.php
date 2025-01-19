31b725dc525d8923077897bc9ec9f53a1aa2b694
vneris236 <vneris@fusionlab.gr>
2025-01-19 14:42:08 +0200

    product url processor adjustment

--- Model/UrlProcessor.php
+++ Model/UrlProcessor.php
@@ -15,11 +15,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 namespace FusionLab\BestPrice\Model;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Psr\Log\LoggerInterface;
 
 class UrlProcessor
 {
@@ -30,76 +34,85 @@ class UrlProcessor
 
     private AdapterInterface $connection;
 
+    private LoggerInterface $logger;
+
     /**
      * @param ProductRepositoryInterface $productRepository
      * @param ResourceConnection $resourceConnection
+     * @param LoggerInterface $logger
      */
     public function __construct(
         ProductRepositoryInterface $productRepository,
-        ResourceConnection $resourceConnection
+        ResourceConnection         $resourceConnection,
+        LoggerInterface            $logger
     ) {
         $this->productRepository = $productRepository;
         $this->connection = $resourceConnection->getConnection();
+        $this->logger = $logger;
     }
 
     /**
-     * @inheritDoc
+     * @param ProductInterface $product
+     * @return string
      */
-    public function getUrl(\Magento\Catalog\Api\Data\ProductInterface $product): string
+    public function getUrl(ProductInterface $product): string
     {
-        $this->_initData();
+        $this->initProductAttributesData();
 
-        if (!isset($this->configurableData[$product->getId()])){
+        if (!isset($this->configurableData[$product->getId()])) {
             return $product->getProductUrl();
         }
 
         $config = $this->configurableData[$product->getId()];
 
         $params = [];
-        foreach ($config['attributes'] as $attributeCode){
+        foreach ($config['attributes'] as $attributeCode) {
             $value = $product->getData($attributeCode);
-            if ($value){
+            if ($value) {
                 $params[] = "{$attributeCode}={$value}";
             }
         }
 
-        $configurableProduct = $this->productRepository->getById($config['parent_id']);
-        $suffix = !empty($params) ? '#' . implode('&', $params) : '';
+        try {
+            $configurableProduct = $this->productRepository->getById($config['parent_id']);
+        } catch (NoSuchEntityException $e) {
+            $this->logger->critical($e, $e->getTrace());
+            return $product->getProductUrl();
+        }
 
+        $suffix = !empty($params) ? '#' . implode('&', $params) : '';
         return $configurableProduct->getProductUrl() . $suffix;
     }
 
     /**
-     * Initialize data
      * @return void
      */
-    private function _initData(): void {
-        if (!$this->configurableData){
+    private function initProductAttributesData(): void
+    {
+        if (!$this->configurableData) {
             $this->configurableData = [];
-            $rawData = $this->_getRawData();
-
+            $rawData = $this->getSimpleAttributesPairs();
 
             foreach ($rawData as $rawDatum) {
                 $configurableData = [
-                    'parent_id' => $rawDatum['configurable_id'],
-                    'attributes' => explode(',', $rawDatum['attributes'])
+                    'attributes' => explode(',', $rawDatum['attributes']),
+                    'parent_id' => $rawDatum['configurable_id']
                 ];
 
                 $simpleIds = explode(',', $rawDatum['simple_ids']);
 
-                foreach ($simpleIds as $simpleId){
+                foreach ($simpleIds as $simpleId) {
                     $this->configurableData[$simpleId] = $configurableData;
                 }
             }
-
         }
     }
 
     /**
-     * Get raw data from select
      * @return array
      */
-    private function _getRawData(): array {
+    private function getSimpleAttributesPairs(): array
+    {
         $select = $this->connection->select()
             ->from(
                 ['cpsl' => $this->connection->getTableName('catalog_product_super_link')],
@@ -116,11 +129,12 @@ class UrlProcessor
                 []
             )
             ->group('cpsl.parent_id')
-            ->columns([
-                'attributes' => new \Zend_Db_Expr('GROUP_CONCAT(DISTINCT eav.attribute_code)'),
-                'simple_ids' => new \Zend_Db_Expr('GROUP_CONCAT(DISTINCT cpsl.product_id)'),
-            ]);
-
+            ->columns(
+                [
+                    'attributes' => new \Zend_Db_Expr('GROUP_CONCAT(DISTINCT eav.attribute_code)'),
+                    'simple_ids' => new \Zend_Db_Expr('GROUP_CONCAT(DISTINCT cpsl.product_id)'),
+                ]
+            );
 
         return $this->connection->fetchAll($select);
     }
